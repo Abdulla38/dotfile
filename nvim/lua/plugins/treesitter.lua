1e0f30fd7285d8791d59d858e0f189211b440ffd
Abdulla Kurmanbaev <abdullakurmanbaev38@gmail.com>
2024-06-27 14:22:31 +0000

    Modified: Editing auto_install and sync_install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "rust", "lua", "python", "css", "jsonc", "json", "toml" },
-  sync_install = true,
-  auto_install = true,
+  ensure_installed = { "markdown", "rust", "lua", "python", "css", "jsonc", "json", "toml" },
+  sync_install = false,
+  auto_install = false,
   highlight = {
     enable = true,
   },
