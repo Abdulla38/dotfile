36fc88e7f2ecadcf01c1e12382bcd9a6ce188272
Abdulla <abdullakurmanbaev38@gmail.com>
2025-02-13 20:06:14 +0600

    style: do {} in one line

--- nvim/lua/core/plugin.lua
+++ nvim/lua/core/plugin.lua
@@ -56,7 +56,5 @@ require("lazy").setup {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-  {
-    "rebelot/kanagawa.nvim",
-  },
+  { "rebelot/kanagawa.nvim" },
 }
