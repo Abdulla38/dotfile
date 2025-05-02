7eadc654eda733eef41e16c770149b2e08ae7e4c
Abdulla <abdullakurmanbaev38@gmail.com>
2025-05-02 15:39:01 +0600

    feat: add telescope.nvim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+local builtin = require('telescope.builtin')
+return {
+  { "nvim-telescope/telescope.nvim",
+    opts = {},
+    branch = '0.1.x',
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    end
+  },
+  { "nvim-lua/plenary.nvim", lazy = true },
+  { "nvim-tree/nvim-web-devicons", opts = {}},
+}
+
